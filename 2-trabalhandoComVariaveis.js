f34f29bf80a973aba7eb9f8ce7227b15e281ec69
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-07-04 21:09:50 -0300

    Fianlizado exercicio 2

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,19 @@
+/* Console.log('Trabalando com variáveis.'); */
+/* JS é case sensitive */
+console.log('Trabalando com variáveis.');
+
+const idade = 31;
+const nome = "Allan Rodrigues dos Santos"
+
+console.log(nome , idade);
+/* 
+mostra que criar a uma variavel com mesmo nome pore um com letra minuscula e outra com letra maiuscula é possivel no JS pois ele é caseSensitive
+Idade = 30;
+console.log(Idade);
+ */
+
+/* 
+Não fazer 
+sem a declaração do tipo da variavel.
+mes = "Janeiro"
+*/
